d3b00b39477bcbc3df9d5aeacdd2559e09095459
vkevroletin <kevroletin@gmail.com>
2011-09-18 17:57:39 +0000

    Разбор входящего запроса

--- dispatcher.pl
+++ dispatcher.pl
@@ -2,22 +2,44 @@ use strict;
 use warnings;
 
 use Plack::Builder;
+use Plack::Response;
+use Plack::Request;
+use JSON;
 
 use Client::Runner;
 
-my $app = sub {
-    my $env = shift;
-    my $status = 200;
-    my $headers = ['Content-Type' => 'text/plain'];
-    my $body = ["Hello :) I will be webgame in the future :)"];
-    return [ $status, $headers, $body ];
+use Data::Dumper;
+
+
+sub parse_request {
+    my ($env) = @_;
+
+    my $res = Plack::Response->new(200);
+    $res->content_type('text/html');
+    my $req = Plack::Request->new($env);
+
+    my $json = $req->raw_body();
+
+    my $data = '';
+    eval {
+        $data = from_json($json)
+    };
+    if ($@) {
+        my $ans = { result => 'badJson',
+                    description => $@ };
+        $res->body(to_json($ans, {pretty => 1}));
+    } else {
+        $res->body(['<pre>', "accepted:\n",
+                    Dumper($json), '</pre>']);
+    }
+
+    $res->finalize();
 };
 
 builder {
     # Include PSGI middleware here
-    enable "Session::Cookie";
 
     mount "/" => \&Client::Runner::run;
-    mount "/engine" => $app;
+    mount "/engine" => \&parse_request;
 };
 
