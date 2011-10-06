70169de2454963186d5c87e76e9951e620f42560
vkevroletin <kevroletin@gmail.com>
2011-10-06 19:49:22 +0000

    Валидация имени пользователя и пароля средствами объектной системы. Moose позволяе объевлять новые типы данных, указывая процедуру валидации и сообщение об ошибке. Пример: subtype 'Username',     as 'Str',     where {         defined $_ &&         $_ =~ /^[A-Za-z][A-Za-z0-9\_\-]{2,15}$/     },     message {         early_response_json({result => 'badUsername'})     }; Технология используется немного неправильно: в сообщениии выбрасывается исключение, хотя должна возвращаться строка с пояснением к ошибке. На самом деле имя пользователя и пароль приходят только с запросами. Поэтому можно смело предположить, что никто, кроме как пользователь не может ошибиться со значением этих полей, а значит в любом случае необходимо будет вернуть ответ с ошибкой. Чтобы избавиться от множества обработок ошибок в коде ответ выбрасывается прямо из процедуры валидации.

--- Game/Model/User.pm
+++ Game/Model/User.pm
@@ -2,51 +2,44 @@ package Game::Model::User;
 use Moose;
 
 use Game::Environment qw(early_response_json);
+use Game::Exception;
 
+use Moose::Util::TypeConstraints;
 
 our @db_index = qw(sid name password);
+
+
+subtype 'Username',
+    as 'Str',
+    where {
+        $_ =~ /^[A-Za-z][A-Za-z0-9\_\-]{2,15}$/
+    },
+    message {
+        early_response_json({result => 'badUsername'})
+    };
+
+subtype 'Password',
+    as 'Str',
+    where {
+        $_ =~ /^.{6,18}$/
+    },
+    message {
+        early_response_json({result => 'badPassword'})
+    };
+
 
 has 'sid' => ( isa => 'Str',
                is  => 'rw',
                required => 0 );
 
-has 'name' => ( isa => 'Str|Undef',
+has 'name' => ( isa => 'Username',
                 is  => 'rw',
                 required => 1,
                   );
 
-has 'password' => ( isa => 'Str|Undef',
+has 'password' => ( isa => 'Password',
                     is  => 'rw',
                     required => 1 );
-
-sub BUILD {
-    _validate_name( $_[0]->{name} );
-    _validate_password( $_[0]->{password} );
-}
-
-before 'name' => sub {
-    my $val = $_[1];
-    defined $val and _validate_name($val)
-};
-
-before 'password' => sub {
-    my $val = $_[1];
-    defined $val and _validate_password($val)
-};
-
-sub _validate_name {
-    unless (defined $_[0] &&
-            $_[0] =~ /^[A-Za-z][A-Za-z0-9\_\-]{2,15}$/) {
-        early_response_json({result => 'badUsername'})
-    }
-}
-
-sub _validate_password {
-    unless (defined $_[0] &&
-            $_[0] =~ /^.{6,18}$/) {
-        early_response_json({result => 'badPassword'})
-    }
-}
 
 
 1
