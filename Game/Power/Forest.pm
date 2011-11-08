28aca4a0c39db33bb5a2aac3657dc3b6dd999155
vkevroletin <kevroletin@gmail.com>
2011-11-08 17:07:57 +0000

    Power Forest

--- Game/Power/Forest.pm
+++ Game/Power/Forest.pm
@@ -8,5 +8,13 @@ with( 'Game::Roles::Power' );
 
 sub power_name { 'forest' }
 
+sub _power_tokens_cnt { 4 }
+
+override 'compute_coins' => sub {
+    my ($self, $regs) = @_;
+    return super() if $self->inDecline();
+    super() + grep { 'forest' ~~ $_->landDescription() } @$regs;
+};
+
 
 1
